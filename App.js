14f2fec0fd0b87a7af11d01fd4a3c199a46d8825
Lalit Garghate <lalit.garghate@gmail.com>
2021-03-28 09:57:55 +0530

    input field, press handler & list rendering(FlatList)

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
+  const [goalsList, setGoalsList] = useState([]);
+  const addGoal = (goal) => {
+    setGoalsList((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), goal },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.root}>
+      <GoalInput addGoal={addGoal} />
+      <FlatList
+        data={goalsList}
+        renderItem={(itemData) => <GoalItem title={itemData.item.goal} />}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    padding: 50,
   },
 });
